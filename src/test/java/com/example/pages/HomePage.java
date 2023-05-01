a71c8ff73c3e145a9d0f34181bfe203c30b1d8aa
Jon Blanchard <jonnathon@me.com>
2023-05-01 16:58:57 -0500

    Implement increment and decrement tests

--- src/test/java/com/example/pages/HomePage.java
+++ src/test/java/com/example/pages/HomePage.java
@@ -4,6 +4,9 @@ import com.example.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.time.Duration;
 
 /**
  * Sample page
@@ -15,6 +18,15 @@ public class HomePage extends BasePage
     super(driver);
   }
 
+  String itemQuantityXpath = "//div[@class='item-quantity']/p";
+  String addButtonXpath = "//button[@class='btn btn-add-item']";
+  String subtractButtonXpath = "//button[@class='btn btn-subtract-item']";
+
+  private String getProductCardXpath(String productName)
+  {
+    return String.format("//div[@class='card-title h5' and text()='%s']/..", productName);
+  }
+
   public boolean isPageLoaded()
   {
     String url = driver.getCurrentUrl();
@@ -23,4 +35,38 @@ public class HomePage extends BasePage
     return (url.contains("localhost") && pageTitle.equals("React App"));
   }
 
+  public int getProductCount(String productName)
+  {
+    String cardXpath = getProductCardXpath(productName);
+    WebElement itemQuantityElement = driver.findElement(By.xpath(cardXpath + itemQuantityXpath));
+    return Integer.parseInt(itemQuantityElement.getAttribute("textContent"));
+  }
+
+  public boolean incrementProductCount(String productName, int amount)
+  {
+    int count = getProductCount(productName);
+    String cardXpath = getProductCardXpath(productName);
+    WebElement addItemButton = driver.findElement(By.xpath(cardXpath + addButtonXpath));
+    for (int i = 1; i <= amount; i++)
+    {
+      addItemButton.click();
+      wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(cardXpath + itemQuantityXpath + "[text()='" + i + "']")));
+    }
+    return (getProductCount(productName) == amount);
+  }
+
+  public boolean decrementProductCount(String productName, int amount)
+  {
+    int count = getProductCount(productName);
+    String cardXpath = getProductCardXpath(productName);
+    WebElement subtractItemButton = driver.findElement(By.xpath(cardXpath + subtractButtonXpath));
+    for (int i = 1; i <= amount; i++)
+    {
+      subtractItemButton.click();
+      if (count - i < 0) continue;
+      wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(cardXpath + itemQuantityXpath + "[text()='" + (count - i) + "']")));
+    }
+    if (count - amount < 0) return getProductCount(productName) == 0;
+    return (getProductCount(productName) == count - amount);
+  }
 }
--- src/test/java/com/example/test/HomePageTests.java
+++ src/test/java/com/example/test/HomePageTests.java
@@ -7,11 +7,30 @@ import org.junit.jupiter.api.Test;
 
 public class HomePageTests extends BaseTest
 {
+    private HomePage homePage = new HomePage(getDriver());
+
     @Test
     public void pageLoadTest()
     {
-        HomePage homePage = new HomePage(getDriver());
         boolean isPageLoaded = homePage.isPageLoaded();
         Assertions.assertTrue(isPageLoaded, "Home page has not loaded correctly");
+    }
+
+    @Test
+    public void productIncrementTest()
+    {
+        String productName = "Blitzing Cap";
+        boolean countIncremented = homePage.incrementProductCount(productName, 5);
+        Assertions.assertTrue(countIncremented, "Product count was not incremented when clicking the button");
+    }
+
+    @Test
+    public void productDecrementTest()
+    {
+        String productName = "Blitzing Cap";
+        boolean initCountUpdated = homePage.incrementProductCount(productName, 3);
+        Assertions.assertTrue(initCountUpdated, "Failed to initialized count as 5 for the given product");
+        boolean countDecremented = homePage.decrementProductCount(productName, 5);
+        Assertions.assertTrue(countDecremented, "Product count was not decremented when clicking the button");
     }
 }
